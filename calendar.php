f92c560eace778d9adf73d3e1a9a71b5f365a53f
Piotr Grochowski <piogrek@gmail.com>
2015-11-11 22:11:50 +0000

    google calendar api integration

--- calendar.php
+++ calendar.php
@@ -1,45 +1,51 @@
 <?php
-	
-	// Set the url of the calendar feed.
-	$url = ''
+include "vendor/autoload.php";
+include "config.php";
 
-	/*****************************************/
+$client = new Google_Client();
+$credentials = $client->loadServiceAccountJson(__DIR__."/googlekeys.json", Google_Service_Calendar::CALENDAR_READONLY);
 
-	// Run the helper function with the desired URL and echo the contents.
-	echo get_url($url);
+$client->setAssertionCredentials($credentials);
+if ($client->getAuth()->isAccessTokenExpired()) {
+    $client->getAuth()->refreshTokenWithAssertion();
+}
 
-	// Define the helper function that retrieved the data and decodes the content.
-	function get_url($url)
-	{
-	    //user agent is very necessary, otherwise some websites like google.com wont give zipped content
-	    $opts = array(
-	        'http'=>array(
-	            'method'=>"GET",
-	            'header'=>"Accept-Language: en-US,en;q=0.8rn" .
-	                        "Accept-Encoding: gzip,deflate,sdchrn" .
-	                        "Accept-Charset:UTF-8,*;q=0.5rn" .
-	                        "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0 FirePHP/0.4rn",
-				"ignore_errors" => true	 //Fix problems getting data
-	        ),
-	        //Fixes problems in ssl 
-		"ssl" => array(
-			"verify_peer"=>false,
-			"verify_peer_name"=>false
-		)
-	    );
-	 
-	    $context = stream_context_create($opts);
-	    $content = file_get_contents($url ,false,$context); 
-	     
-	    //If http response header mentions that content is gzipped, then uncompress it
-	    foreach($http_response_header as $c => $h)
-	    {
-	        if(stristr($h, 'content-encoding') and stristr($h, 'gzip'))
-	        {
-	            //Now lets uncompress the compressed data
-	            $content = gzinflate( substr($content,10,-8) );
-	        }
-	    }
-	     
-	    return $content;
-	}
+$service = new Google_Service_Calendar($client);
+
+// Print the next 10 events on the user's calendar.
+$calendarId = CAL_ID;
+
+$optParams = [
+    'maxResults'   => 15,
+    'orderBy'      => 'startTime',
+    'singleEvents' => true,
+    'timeMin'      => date('c'),
+];
+//$calendars = $service->calendarList->listCalendarList();
+///** @type Google_Service_Calendar_Calendar $cal */
+//foreach($calendars->getItems() as $cal) {
+// var_dump($cal->getId());
+//}
+$results = $service->events->listEvents($calendarId, $optParams);
+//var_dump($results->count());
+$events = [];
+
+/** @type Google_Service_Calendar_Event $event */
+foreach ($results as $event) {
+//var_dump($event);
+//    var_dump($event->getSummary());
+    $time = $event->getStart()->getDateTime()
+        ? new DateTime($event->getStart()->getDateTime())
+        : new DateTime($event->getStart()->getDate());
+
+    $start = $event->getStart()->getDateTime() ? $time->format('g:ia | D jS') : $time->format('D jS');
+
+    $events[] = [
+        'summary'  => $event->getSummary(),
+        'start'    => $start,
+        'end'      => $event->getEnd()->getDateTime(),
+        'location' => $event->getLocation(),
+    ];
+}
+header('Content-Type: application/json');
+echo json_encode($events);
